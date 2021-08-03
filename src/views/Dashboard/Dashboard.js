cfdbf072bf41fa5e2bd59931cc49045447a28c51
otherRick <drckbr@gmail.com>
2021-08-03 20:06:14 -0300

    add Link component

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import Input from "../../components/Input/Input";
 import DatePicker from "../../components/DatePicker/DatePicker";
+import Link from "../../components/Link/Link";
 
 function Dashboard() {
   return (
     <div>
       <Input label="exemplo" placeholder="o que eu quiser" />
       <DatePicker label="Foi hoje ?" />
+      <Link to="/category">Criar Categoria</Link>
       {/* select - futuro */}
     </div>
   );
