978c38a74bfe123b233b463319af8adf418cd7ee
otherRick <drckbr@gmail.com>
2021-08-03 20:17:17 -0300

    add button and component and confirm button

--- src/views/Category/Category.js
+++ src/views/Category/Category.js
@@ -1,7 +1,15 @@
+import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
+import Link from "../../components/Link/Link";
 
 const Category = () => {
-  return <Input />;
+  return (
+    <div>
+      <Input />
+      <Link to="/">Home</Link>
+      <Button>Confirm </Button>
+    </div>
+  );
 };
 
 export default Category;
